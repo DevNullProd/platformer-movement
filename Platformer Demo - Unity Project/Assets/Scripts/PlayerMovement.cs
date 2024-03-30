b1924c6e2f7233ccd671e9f5a1ee886c9b4cf49b
Mo Morsi <mo@morsi.org>
2024-03-30 10:36:28 -0400

    Include change missed in last patch

--- Platformer Demo - Unity Project/Assets/Scripts/PlayerMovement.cs
+++ Platformer Demo - Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -114,19 +114,8 @@ partial class PlayerMovement : MonoBehaviour
   }
 
   private void FixedUpdate(){
-    // Handle Run
-    if(!IsDashing){
-      if(IsWallJumping)
-        Run(Data.wallJumpRunLerp);
-      else
-        Run(1);
-
-    }else if(_isDashAttacking)
-      Run(Data.dashEndRunLerp);
-
-    // Handle Slide
-    if(IsSliding)
-      Slide();
+    FixedUpdateRun();
+    FixedUpdateSlide();
   }
 
   /////////////////////////////////////////////////////
